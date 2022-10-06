57576b3ad3c15eb08b90f522ee2bc9950f47390a
Andy Beloff <andrey_budda_belov@mail.ru>
2022-10-06 09:11:08 +0500

    Add comments and reports with test

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -1,5 +1,12 @@
+import kotlin.random.Random
+
 object WallService {
-    var posts = emptyArray<Post>()
+    private var posts = emptyArray<Post>()
+    private var comments = emptyArray<Comment>()
+    private var reports = emptyArray<Report>()
+
+    val testReport get() = reports.last()
+
     private var lastId: Int = 1
 
     fun clear() {
@@ -16,6 +23,39 @@ object WallService {
         return posts.last()
     }
 
+    fun createComment(postId: Int?, comment: Comment): Comment {
+        if (!posts.any { it.id == postId }) {
+            throw PostNotFoundException("Пост с id $postId не найден")
+        }
+        for (post in posts) {
+            if (post.id == postId) {
+                post.comments = comment
+                comments += comment
+                comment.id = comments.indexOf(comment)
+            }
+        }
+        return comments.last()
+    }
+
+    fun addReport(commentId: Int, reason: Int): Report {
+        if (!comments.any { it.id == commentId }) {
+            throw CommentNotFoundException("Комментарий с id $commentId не найден")
+        }
+        when (reason) {
+            in 0..8 -> {
+                val newReport = Report(
+                    Random.nextInt(Int.MAX_VALUE),
+                    Random.nextInt(Int.MAX_VALUE),
+                    reason
+                )
+                reports += newReport
+                return reports.last()
+            }
+            else -> throw ReasonNotFoundException("Ошибка: $reason отсутствует в базе")
+        }
+
+    }
+
     fun update(post: Post): Boolean {
         for (currentPost in posts) {
             if (post.id == currentPost.id) {
@@ -27,3 +67,7 @@ object WallService {
     }
 
 }
+
+class PostNotFoundException(message: String) : RuntimeException(message)
+class CommentNotFoundException(message: String) : RuntimeException(message)
+class ReasonNotFoundException(message: String) : RuntimeException(message)
